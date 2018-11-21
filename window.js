d9e0fdd6ac9a5ee3b60bda4d466d26367b12abd1
0100001001000010 <bblore.sh@mysummitps.org>
2018-11-20 17:13:31 -0800

    Stuff now seems to scale properly

---
+++ window.js
@@ -0,0 +1,23 @@
+//This is the window management code.
+/* This function resizes the sidebar-content and main-container containers when
+ * the application is started or the window is resized.
+ *
+ * Important: If you change the constants in this function, you must also change
+ * the appropriate constants in the stylesheet.
+ */
+function resizeContainers() {
+    var em = parseFloat(getComputedStyle(document.body).fontSize); //Calculate how many pixels an em is
+    document.getElementById("sidebar-content").style.maxHeight = //This is the main content of the sidebar
+        String(innerHeight - em * 5) + "px";
+    document.getElementById("main-content").style.maxHeight = //This is the main content of the page
+        String(innerHeight - em * 2.5) + "px";
+}
+// This code is run whenever the window is resized.
+addEventListener("resize", function () {
+    resizeContainers();
+});
+//This code is run when the application is started.
+addEventListener("load", function () {
+    resizeContainers();
+});
+console.log("Window management code loaded");
